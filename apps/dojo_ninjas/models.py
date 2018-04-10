6b95a8b049d4d7465cf7e0a1199fdbe674c20aba
Gerardo <jimgerar@amazon.com>
2018-04-10 15:44:35 -0600

    shell assignments all completed

---
+++ apps/dojo_ninjas/models.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class Dojo (models.Model):
+    name = models.CharField(max_length = 255)
+    city = models.CharField(max_length = 255)
+    state = models.CharField(max_length = 255)
+    desc = models.TextField(default='')
+
+    def __str__ (self):
+        return self.name
+
+    def __repr__ (self):
+        return self.name
+
+class Ninja (models.Model):
+    first_name = models.CharField(max_length = 255)
+    last_name = models.CharField(max_length = 255)
+    dojo = models.ForeignKey (Dojo, related_name='ninjas')
+
+    def __str__ (self):
+        return self.first_name
